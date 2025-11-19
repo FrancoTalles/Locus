c0c506d3f46781737598a8fbe4c83e90a3324d1f
Victor Hugo Mendes <victor.mendesouza@gmail.com>
2025-11-19 07:23:27 -0400

    crud usuarios, verifica

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioController } from './usuario/usuario.controller';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsuariosModule],
+  controllers: [AppController, UsuarioController],
   providers: [AppService],
 })
 export class AppModule {}
